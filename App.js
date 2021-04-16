3f950d31c7d415f717ed2c3ee56180fcaf55aa3d
jose sanchez <txami6996@gmail.com>
2021-04-16 16:06:57 +0200

    Componentes React Native V1

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Campobase from './componentes/CampobaseComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>adios</Text>
+    <View>
+      <Campobase/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#00BFFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
