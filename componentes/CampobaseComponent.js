8d88e2b612518bc275a1bf7fa08cd1da2a71bbdf
jose sanchez <txami6996@gmail.com>
2021-04-18 20:24:07 +0200

    Drawer Navigation V1

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -5,8 +5,26 @@ import DetalleExcursion from './DetalleExcursionComponent';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Home from './HomeComponent';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
+function DrawerNavegador() {
+  return (
+    <Drawer.Navigator
+      drawerStyle={{
+        backgroundColor: '#c2d3da',
+      }}
+      initialRouteName="Home"
+    >
+      <Drawer.Screen name="Home" component={HomeNavegador} />
+      <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
+    </Drawer.Navigator>
+  );
+}
 
 function CalendarioNavegador() {
   return (
@@ -36,6 +54,27 @@ function CalendarioNavegador() {
     </Stack.Navigator>
   );
 }
+function HomeNavegador() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      headerMode="screen"
+      screenOptions={{
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: '#015afc' },
+        headerTitleStyle: { color: '#fff' },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Campo Base',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 class Campobase extends Component {
 
@@ -43,12 +82,12 @@ class Campobase extends Component {
 
     return (
       <NavigationContainer>
-        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-          <CalendarioNavegador />
+        <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+          <DrawerNavegador />
         </View>
-      </NavigationContainer>   
+      </NavigationContainer>
     );
-}
+  }
 }
 
 export default Campobase;
---
+++ componentes/HomeComponent.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Text, ScrollView, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { EXCURSIONES } from '../comun/excursiones';
+import { CABECERAS } from '../comun/cabeceras';
+import { ACTIVIDADES } from '../comun/actividades';
+
+function RenderItem(props) {
+    
+        const item = props.item;
+        
+        if (item != null) {
+            return(
+                <Card>
+                    <Card.Title>{item.nombre}</Card.Title>
+                    <Card.Divider/>
+                    <Card.Image source={require('./imagenes/40Años.png')}></Card.Image>
+                    <Text style={{margin: 20}}>
+                        {item.descripcion}
+                    </Text>
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
+}
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          excursiones: EXCURSIONES,
+          cabeceras: CABECERAS,
+          actividades: ACTIVIDADES
+        };
+    }
+
+    render() {
+        
+        return(
+            <ScrollView>
+                <RenderItem item={this.state.cabeceras.filter((cabecera) => cabecera.destacado)[0]} />
+                <RenderItem item={this.state.excursiones.filter((excursion) => excursion.destacado)[0]} />
+                <RenderItem item={this.state.actividades.filter((actividad) => actividad.destacado)[0]} />
+            </ScrollView>
+        );
+    }
+}
+
+export default Home;
