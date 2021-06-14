8f15e8de280a6a75f20f7f901337fc59af1bdc5c
jose sanchez <txami6996@gmail.com>
2021-06-14 12:28:12 +0200

    Comentarios Upload

--- redux/comentarios.js
+++ redux/comentarios.js
@@ -1,18 +1,34 @@
 import * as ActionTypes from './ActionTypes';
-
-export const comentarios = (state = { errMess: null, comentarios:[]}, action) => {
+import { baseUrl } from '../comun/comun'
+export const comentarios = (state = { errMess: null, comentarios: [] }, action) => {
   switch (action.type) {
     case ActionTypes.ADD_COMENTARIOS:
-      return {...state, errMess: null, comentarios: action.payload};
+      return { ...state, errMess: null, comentarios: action.payload };
 
     case ActionTypes.COMENTARIOS_FAILED:
-      return {...state, errMess: action.payload};
-    
-      case ActionTypes.ADD_COMENTARIO:
+      return { ...state, errMess: action.payload };
+
+    case ActionTypes.ADD_COMENTARIO:
       //add id
-      var commen=action.payload;
-      commen.id=state.comentarios.length;
-      return {...state, errMess: null, comentarios: state.comentarios.concat(commen)};
+      var commen = action.payload;
+      commen.id = state.comentarios.length;
+      let errmsg = null;
+      fetch(baseUrl + 'comentarios/' + commen.id + '.json', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(commen
+          // firstParam: 'yourValue',
+          //secondParam: 'yourOtherValue'
+        )
+      })
+        .then(response => response.json())
+        .catch((error) => {
+          consolelog(error);
+          errmsg = error.message;
+        });
+      return { ...state, errMess: errmsg, comentarios: state.comentarios.concat(commen) };
 
     default:
       return state;
