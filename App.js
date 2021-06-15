2c3e9b547afc5d54863cb4d1140ee27dec1cb130
jose sanchez <txami6996@gmail.com>
2021-06-15 11:24:23 +0200

    Memoria Persistente

--- App.js
+++ App.js
@@ -3,21 +3,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Campobase from './componentes/CampobaseComponent';
 import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
-
-const store = ConfigureStore();
-
+//import { ConfigureStore } from './redux/configureStore';
+//const store = ConfigureStore();
+import { store, persistor } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/integration/react';
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Campobase/>
-        <StatusBar style="auto" />
-      </View>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.container}>
+          <Campobase />
+          <StatusBar style="auto" />
+        </View>
+      </PersistGate>
     </Provider>
   );
 }
-
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,9 +6,14 @@ import { comentarios } from './comentarios';
 import { cabeceras } from './cabeceras';
 import { actividades } from './actividades';
 import { favoritos } from './favoritos';
-
-
-export const ConfigureStore = () => {
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const favPersistConfig = {
+    key: 'root',
+    storage: AsyncStorage ,
+    whitelist: ['excursiones', 'favoritos'],
+  }
+/*export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             excursiones,
@@ -22,4 +27,19 @@ export const ConfigureStore = () => {
     );
 
     return store;
-}
+}*/
+const rootReducer = combineReducers({
+    excursiones,
+    comentarios,
+    cabeceras,
+    actividades,
+    favoritos,
+})
+const persistedReducer = persistReducer(favPersistConfig, rootReducer)
+
+export const store = createStore(
+    persistedReducer,
+    applyMiddleware(thunk, logger)
+);
+
+export const persistor = persistStore(store); 
